ea50a765eeed3b939ac376039ede968e54431db7
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-02-01 22:40:19 +0530

    Ex4: #TC2: test for response.result, passed

--- Ex4/4_dir.test.js
+++ Ex4/4_dir.test.js
@@ -1,9 +1,18 @@
 const Server = require('./4_dir');
+const fs = require('fs');
 
 describe('test server: ', () => {
   test('Should return statusCode: ', (done) => {
     Server.inject('/foo/bar/baz/file.html', (response) => {
       expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+
+  test('Should return the body: ', (done) => {
+    const data = fs.readFileSync('/Users/vishalvasnani/MDL/training/makemehapi_Ex/Ex4/public/file.html');
+    Server.inject('/foo/bar/baz/file.html', (response) => {
+      expect(response.result.toString()).toBe(data.toString());
       done();
     });
   });
