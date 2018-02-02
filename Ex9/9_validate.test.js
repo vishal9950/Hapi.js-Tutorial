b6cd4ec46047cf0893b4788c482179f888ba611e
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-02-02 08:44:12 +0530

    Ex9: #TC2: test result, indian

--- Ex9/9_validate.test.js
+++ Ex9/9_validate.test.js
@@ -2,8 +2,15 @@ const Server = require('./9_validate');
 
 describe('test status code: ', () => {
   test('should return 200 status code: ', (done) => {
-    Server.inject('http://localhost:8080/chicken/breed=indian', (response) => {
+    Server.inject('http://localhost:8080/chicken/indian', (response) => {
       expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+
+  test('should return result indian: ', (done) => {
+    Server.inject('http://localhost:8080/chicken/indian', (response) => {
+      expect(response.result).toBe('indian');
       done();
     });
   });
