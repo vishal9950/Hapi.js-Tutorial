4dd72eaded5cc1a9ce5a430341c4ed711fb16a9d
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-02-04 23:08:48 +0530

    Ex7: #TC2: return result

--- Ex7/7_help.test.js
+++ Ex7/7_help.test.js
@@ -1,9 +1,18 @@
 const Server = require('./7_help');
+const fs = require('fs');
 
 describe('Test server: ', () => {
   test('Should return statusCode 200: ', (done) => {
     Server.inject('/', (response) => {
       expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+
+  test('Should return response ', (done) => {
+    const data = fs.readFileSync('./Ex7/templates/testFile.html');
+    Server.inject('/?name=abcd&suffix=!', (response) => {
+      expect(response.result.toString()).toBe(data.toString());
       done();
     });
   });
